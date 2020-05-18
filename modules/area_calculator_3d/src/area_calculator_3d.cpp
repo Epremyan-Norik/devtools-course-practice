6929169811daf8218942862a5fdef86355b32dcf
Epremyan Norik <epnorik@yandex.ru>
2020-05-18 21:34:39 +0300

    Trying to build

--- modules/area_calculator_3d/src/area_calculator_3d.cpp
+++ modules/area_calculator_3d/src/area_calculator_3d.cpp
@@ -8,13 +8,6 @@
 #include <string>
 #include <sstream>
 
-bool area_calculator_3d::validateNumberOfArguments(int argc, const char** argv) {
-    if (argc == 1) {
-        help(argv[0]);
-        return false;
-    } else if (argc != 6) {
-        help(argv[0], "ERROR: Should be 5 arguments.\n\n");
-        return false;
-    }
-    return true;
-}
+/*bool area_calculator_3d::validateNumberOfArguments(int argc, const char** argv) {
+
+}*/
