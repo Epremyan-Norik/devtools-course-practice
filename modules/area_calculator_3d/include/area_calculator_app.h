2bba960ffa104a76795ab035a0cc015c6b2b2769
Epremyan Norik <epnorik@yandex.ru>
2020-06-06 00:47:59 +0300

    Update area_calculator_app.h

--- modules/area_calculator_3d/include/area_calculator_app.h
+++ modules/area_calculator_3d/include/area_calculator_app.h
@@ -1,10 +1,10 @@
 // Copyright 2020 Epremyan Norik
 #ifndef MODULES_AREA_CALCULATOR_3D_INCLUDE_AREA_CALCULATOR_APP_H_
 #define MODULES_AREA_CALCULATOR_3D_INCLUDE_AREA_CALCULATOR_APP_H_
+#include <include/calculator.h>
 #include <string>
 #include <iostream>
 #include <vector>
-#include <include/calculator.h>
 
 class area_calculator_app {
  public:
